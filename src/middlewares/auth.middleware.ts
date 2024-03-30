f4be12e2a52a41ddc940d2293fd563af1bc1393b
Mohommed Muthahhar <muthahhar@echonlabs.com>
2024-03-30 12:59:50 +0530

    auth middlware updated

--- src/middlewares/auth.middleware.ts
+++ src/middlewares/auth.middleware.ts
@@ -1,17 +1,25 @@
 import { Request, Response, NextFunction } from "express";
 import jwt from "jsonwebtoken";
+import dotenv from "dotenv";
 
-const JWT_SECRET = process.env.JWT_SECRET;
+dotenv.config();
+
+const JWT_SECRET = process.env.JWT_SECRET as string;
 
 const verifyToken = (req: Request, res: Response, next: NextFunction) => {
-  const token = req.headers["authorization"]?.split(" ")[1];
-  if (!token) return res.status(401).json({ message: "Unauthorized" });
-
-  jwt.verify(token, JWT_SECRET as string, (err: any, decoded: any) => {
-    if (err) return res.status(401).json({ message: "Invalid token" });
-    req.body.userId = decoded.id;
-    next();
-  });
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.split(" ")[0] === "Bearer"
+  ) {
+    const token = req.headers.authorization.split(" ")[1];
+    if (!token) return res.status(401).json({ message: "Unauthorized" });
+    console.log(JWT_SECRET);
+    jwt.verify(token, JWT_SECRET, (err: any, decoded: any) => {
+      if (err) return res.status(401).json({ message: "Invalid token" });
+      req.body.userId = decoded.userId;
+      next();
+    });
+  }
 };
 
 export default verifyToken;
