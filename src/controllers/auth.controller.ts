5889db4bd52e70f385f88c5e36b8b5aaecdc55f2
Mohommed Muthahhar <muthahhar@echonlabs.com>
2024-03-30 12:57:53 +0530

    auth relaed functions added

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -3,18 +3,31 @@ import jwt from "jsonwebtoken";
 import bcrypt from "bcrypt";
 import UserModel from "../models/user.model";
 
+/**
+ * This method will be used to authenticate against a valid username and a password
+ * @param req {express.Request} which uses to accept the request from origin
+ * @param res {express.Response} which uses for sending the response back to request origin
+ * @returns Express response object
+ * @author Mohommed Muthahhar
+ */
 export const login = async (req: Request, res: Response) => {
   const { username, password } = req.body;
   try {
     const user = await UserModel.findOne({ username });
     if (!user) {
-      return res.status(401).json({ message: "Invalid email or password" });
+      return res.status(401).json({ message: "Invalid username or password" });
     }
     const isPasswordValid = await bcrypt.compare(password, user.password);
     if (!isPasswordValid) {
-      return res.status(401).json({ message: "Invalid email or password" });
+      return res.status(401).json({ message: "Invalid username or password" });
     }
-    const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET!);
+    const token = jwt.sign(
+      { userId: user._id },
+      process.env.JWT_SECRET as string,
+      {
+        expiresIn: "1h",
+      }
+    );
     res.json({ token });
   } catch (err) {
     res.status(500).json({ message: "Server error" });
