d186fce68679ffc60738d97f18bdcddbc5fca4b4
Mohommed Muthahhar <muthahhar@echonlabs.com>
2024-03-30 13:01:56 +0530

    validate request body added

---
+++ src/middlewares/update-validate.middleware.ts
@@ -0,0 +1,24 @@
+import { Request, Response, NextFunction } from "express";
+import Ajv, { ValidateFunction } from "ajv";
+import updateSchema from "../schemas/updateUser.schema.json";
+
+const ajv = new Ajv();
+
+const validateRequest = (schema: object): ValidateFunction => {
+  return ajv.compile(schema);
+};
+
+export const validateRequestBody = (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const validate = validateRequest(updateSchema);
+  const isValid = validate(req.body);
+  if (!isValid) {
+    return res
+      .status(400)
+      .json({ error: "Invalid request body", errors: validate.errors });
+  }
+  next();
+};
